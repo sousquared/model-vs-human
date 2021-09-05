abff3b2b41ff785d270485ef6cd643960f6aadb7
sousquared <sou.yoshihara.72e@gmail.com>
2021-09-06 08:21:12 +0900

    Edit plot.

--- eval_bt/plotting_definition.py
+++ eval_bt/plotting_definition.py
@@ -27,15 +27,18 @@ def plotting_definition_template(df):
 
     decision_makers = []
 
-    decision_makers.append(DecisionMaker(name_pattern="resnet50",
+    decision_makers.append(DecisionMaker(name_pattern="alexnet",
                            color=rgb(65, 90, 140), marker="o", df=df,
-                           plotting_name="ResNet-50"))
-    decision_makers.append(DecisionMaker(name_pattern="bagnet33",
-                           color=rgb(110, 110, 110), marker="o", df=df,
-                           plotting_name="BagNet-33"))
-    decision_makers.append(DecisionMaker(name_pattern="simclr_resnet50x1",
-                           color=rgb(210, 150, 0), marker="o", df=df,
-                           plotting_name="SimCLR-x1"))
+                           plotting_name="AlexNetNet"))
+    # decision_makers.append(DecisionMaker(name_pattern="resnet50",
+    #                        color=rgb(65, 90, 140), marker="o", df=df,
+    #                        plotting_name="ResNet-50"))
+    # decision_makers.append(DecisionMaker(name_pattern="bagnet33",
+    #                        color=rgb(110, 110, 110), marker="o", df=df,
+    #                        plotting_name="BagNet-33"))
+    # decision_makers.append(DecisionMaker(name_pattern="simclr_resnet50x1",
+    #                        color=rgb(210, 150, 0), marker="o", df=df,
+    #                        plotting_name="SimCLR-x1"))
     decision_makers.append(DecisionMaker(name_pattern="subject-*",
                            color=rgb(165, 30, 55), marker="D", df=df,
                            plotting_name="humans"))
