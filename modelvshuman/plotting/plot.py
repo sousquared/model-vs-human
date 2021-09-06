99571536df6194cf2b0e829639541ca1b3855878
sousquared <sou.yoshihara.72e@gmail.com>
2021-09-06 10:11:06 +0900

    (debug) Change plot types.

--- modelvshuman/plotting/plot.py
+++ modelvshuman/plotting/plot.py
@@ -84,7 +84,7 @@ def plot(plot_types,
         os.makedirs(result_dir)
 
     crop_dirs = [result_dir]
-    plot_types = ['benchmark-barplot', 'scatterplot']  # debug
+    plot_types = ['scatterplot']  # debug
 
     for plot_type in plot_types:
         if dataset_names is None:
